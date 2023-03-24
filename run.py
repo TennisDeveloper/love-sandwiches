152c889159803212da14406f9151fb8d99308ce0
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2023-03-24 05:43:44 +0000

    Refactoring of the sheet update functions into one.

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
-
+]
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -17,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -52,50 +54,47 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
 
-    print("updating sales worksheet... \n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+def update_worksheet(data, worksheet):
+    """ 
+    Receives a list of integers to be inserted into a worksheet.
+    Update a relevant worksheet with the data provided.
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet }worksheet updated successfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
-    Compare sales with stock and calculate the surplus for each item type. 
+    Compare sales with stock and calculate the surplus for each item type.
     The surplus is defined as the sales figure subtracted from the stock:
-    -positive surplus indicates waste
-    -negative surplus indicates extra made when stock was sold out
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-
-    print("Calculating surplus data ... \n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(f"stock row: {stock_row}")
-    print(f"sales row: {sales_row}")
-
+    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
+
     return surplus_data
 
 
 def main():
-    """"
+    """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
-    
-    
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
-
